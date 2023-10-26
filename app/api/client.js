bb76aaece22b96cb941035742e34b12d1fcd9c48
chris b <christopherbrowning@Christophers-MacBook-Pro.local>
2023-10-26 14:09:55 +0200

    implemented React component fetching and displying listings data from listingsAPI

--- app/api/client.js
+++ app/api/client.js
@@ -1,5 +1,6 @@
 import { create } from "apisauce";
 
+// Calling API with apisauce
 const apiClient = create({
   baseURL: "http://192.168.178.98:8082/api",
 });
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
+import listingsApi from "../api/listings";
 
-const listings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+const [listing, setListings] = useState([]);
+//state varibles
+
+useEffect(() => {
+  loadListings();
+}, []);
+
+// Fetching the data from listingsAPI
+const loadListings = async () => {
+  const response = await listingsApi.getListings();
+  setListings(response.data);
+};
 
 function ListingsScreen({ navigation }) {
   return (
